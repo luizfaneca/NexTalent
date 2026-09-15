83c1b3afdd7abd9bc81f6e531ef967df732221bc
luizfaneca <luizfaneca@gmail.com>
2026-09-15 17:07:10 -0300

    Update reports.js

--- reports.js
+++ reports.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const db = require('../db/init');
+const db = require('./init');
 const { requireAuth, requireRole } = require('../middleware/auth');
 
 const router = express.Router();
